743dc7ab3761bba0a4e965969eaaa70fd43eff83
Chloe Chen <cc126@rice.edu>
2024-03-06 14:59:39 -0600

    kernelstart: finished draft of vector interrupt table

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include <terminals.h>
 #include <hardware.h>
 
+int **interruptVector[TRAP_VECTOR_SIZE];
+
 void TrapKernelHandler(ExceptionInfo *info);
 void TrapClockHandler(ExceptionInfo *info);
 void TrapIllegalHandler(ExceptionInfo *info);
@@ -23,8 +25,23 @@ void TrapTTYTransmitHandler(ExceptionInfo *info);
     */
 extern void KernelStart(ExceptionInfo *info, unsigned int pmem_size, void **orig_brk, char **cmd_args)
 {
-    // Initialize interrupt vector table
-    int(*interruptVector[TRAP_VECTOR_SIZE])
+    /* Initialize interrupt vector table */
+    interruptVector = (int *) malloc(TRAP_VECTOR_SIZE * sizeof(int*)); // allocate space in physical memory
+    
+    // assign handler functions to indices
+    interruptVector[TRAP_KERNEL] = TrapKernelHandler;
+    interruptVector[TRAP_CLOCK] = TrapClockHandler;
+    interruptVector[TRAP_ILLEGAL] = TrapIllegalHandler;
+    interruptVector[TRAP_MEMORY] = TrapMemoryHandler;
+    interruptVector[TRAP_MATH] = TrapMathHandler;
+    interruptVector[TRAP_TTY_RECEIVE] = TrapTTYReceiveHandler;
+    interruptVector[TRAP_TTY_TRANSMIT] = TrapTTYTransmitHandler;
+    
+    // fill in rest of empty slots
+    for (size_t i = TRAP_DISK; i < TRAP_VECTOR_SIZE; i++)
+    {
+        interruptVector[i] = NULL;
+    }
 
     // Build a boolean array that keeps track of free pages
     // Build 1 initial page table
